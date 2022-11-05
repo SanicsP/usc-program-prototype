ea5604122576623056e13ae49a1a2f7285e72eab
Sanics'P <sanics290@gmail.com>
2022-11-05 17:53:33 +0100

    ajout de la documentation sur les classes recorder et session

--- include/recorder.hpp
+++ include/recorder.hpp
@@ -25,54 +25,92 @@ public:
     recorder(std::shared_ptr<sf::RectangleShape> rect_ptr , sf::RenderWindow& win_ref);
     
    
-
+    /**
+     * @brief mise à jour de l'instance 
+     * @note lors de l'appelle à cette fonction les calculs pour les statistiques sont effectués dans le but
+     * d'avoir une évolution en temps réel
+    */
     void update() ;
 
+    /**
+    * @brief verifie si le rectangle pointé a changé de position sur la fenêntre 
+    * @return true si c'est le cas et false dans le cas contraire 
+    */
     bool rect_moved() ;
 
+    /**
+     * @brief donne la moyenne des vitesses de l'utilisateur à partir du tableau de vitesses 
+     * @return la vitesse moyenne de l'utilisateur
+    */
     double get_speed_mean() const ;
 
+    /**
+     * @brief donne la vitesse moyenne de l'utilisateur entre le changement de position de la cible et le 
+     * click sur cette dernière 
+    */
     double get_curret_speed() const ;
 
+    /**
+     * @brief affect un pointeur à l'instance pour pouvoir gérer une zone de texte
+     * @param box_ptr l'adresse de la boîte de texte
+    */
     void set_txt_box(text_box* box_ptr);
 
+    /// @brief renitialise les stats de l'utilisateur 
     void restart();
 
+    /// @brief renitialise le point de temps après la pause , afin que le temps entre la pause ne soit pas compté 
     void reset_time();
 
 
 private:
-
+    /// @brief compteur du nombre de clicks 
     uint32_t m_counter;
 
+    /// @brief pointeur vers la boite de la cible affin de capter un mouvement 
     std::shared_ptr<sf::RectangleShape> m_rect_ptr;
 
+    /// @brief position du rectangle 
     sf::Vector2f m_record_rect_pos;
     
+    /// @brief ancienne position du rectangle 
     sf::Vector2f m_record_old_rect_pos; 
 
-
+    /// @brief la distance entre la souris et le rectangle 
     double m_record_distbtw_mouse_rect;
 
+    /// @brief la duree entre le repositionnement du rectangle et le click de l'utilisateur 
     r_duration m_click_duration;
     
+    /// @brief momment dans le temps : celui où le rectangle s'est repositionné 
     r_t_point m_start_moment;
     
+    /// @brief momment dans le temps : celui où le joueur a cliqué sur le rectangle 
     r_t_point m_click_moment;
 
+    /// @brief reference sur la fenêtre 
     sf::RenderWindow& m_win_ref;
     
+    /// @brief vecteur de valeurs de vitesse moyenne 
     std::vector<double> m_recorder_speed_array;
 
-
+    /// @brief pointeur vers la boîte de dialogue à afficher 
     text_box* m_stat_box;
 
+    /**
+     * @brief enregistre la distance ente la souris et le rectangle par rapport à la fenêtre 
+     * @param win la fenêtre de référece 
+    */
     void record_dist_btwnMR(sf::RenderWindow& win);
 
+    /// @brief enregistre la durée entre le repositionnement du rectangle et le click de ce dernier 
     void record_duration();
 
+    /// @brief enregistre la vitesse moyenne de l'utilisateur par rapport à la distance rectangle/souris et la durée 
+    /// repositionnement/click 
     double record_speed();
 
+    /// @brief enregistre la vitesse moyenne courante dans un vecteur de vitesses 
     void store_speed();
 
 
--- include/session.hpp
+++ include/session.hpp
@@ -5,6 +5,13 @@
 #include "recorder.hpp"
 #include "manager.hpp"
 #include <iostream>
+
+/// @file session.hpp
+
+/**
+ * @enum step 
+ * @brief ennumérations symbolisant les différents états du programme
+*/
 enum class step {
 
     START = 0 ,
@@ -13,32 +20,75 @@ enum class step {
     END = 3
 };
 
+/**
+ * @brief represente le programme lui même et ses différents états 
+ * @note cette classe est en relation avec recorder et manager , cette classe actualise ses dernières afin 
+ * que l'utilisateur puisse recommencer ses stats avec recorder , et que manager s'actualise également 
+ * il gère aussi la fenêtre et les évennements avec la fonction main
+*/
 class session { 
 
 public:
-
+    /**
+     * @brief constructeur de la classe afin d'initiliser les instances 
+     * @param rec adresse d'un objet de type recorder : c'est le recorder à gérer 
+     * @param man adresse de manager : le manager à gérer 
+     * @param win_ref la fenêtre à gérer 
+     * @param event_ref l'evennement à gérer 
+    */
     session(recorder* rec , manager* man , sf::RenderWindow& win_ref , sf::Event& event_ref);
     
+    /**
+     * @brief accesseur pour changer les états de la session
+    */
     void set_stat(step stat);
+    
+    /**
+     * @brief revoi l'état courant du programme
+    */
     step stat() const;
-
+    
+    /**
+     *@brief renvoi l'ancien état du programme
+    */
     step last_stat() const;
 
+    /**
+     * @brief fonction appelée lorsque le programme est à l'etat step::START
+    */
     void s_start();
+
+    /**
+     * @brief fonction appelée lorsque le programme est à l'etat step::RESTART
+    */
     void s_restart();
+    
+    /**
+     * fonction appelée lorsque le programme est à l'etat step::END
+    */
     void s_end();
+    
+    /**
+     * fonction appelée lorsque le programme est à l'etat step::PAUSE
+    */
     void s_pause();
 
 
 
 private:
     
+    /// @brief l'etat courant du programme 
     step m_step;
+    /// @brief l'ancien etat du programme
     step m_last_step;
+    /// @brief pointeur sur un recorder
     recorder* recorder_ptr;
+    /// @brief pointeur sur un manager 
     manager* manager_ptr;
 
-    sf::RenderWindow& m_win_ref;
+    /// @brief reference sur une fenêtre 
+    sf::RenderWindow& m_win_ref;    
+    /// @brief reference sur un evennement 
     sf::Event& m_event_ref;
 
 };
